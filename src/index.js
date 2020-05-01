308bc4661bcb4499ab89a72221007e046b3a1fda
Apa6 <alim.gazdieff@gmail.com>
2020-05-01 13:37:37 +0500

    36,37 (refactoring State.js,index.js; add all function in object store and add bind context in index.js)

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {addPost, subscribe, textareaChanges} from "./redux/State";
-import state from "./redux/State";
+
+import store from "./redux/State";
 
 let rerenderEntireTree = () => {
 	ReactDOM.render(
 		<BrowserRouter>
-			<App state={state} addPost={addPost} currentText={textareaChanges}/>
+			<App state={store.getState()} addPost={store.addPost.bind(store)} currentText={store.textareaChanges.bind(store)}/>
 		</BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderEntireTree(state)
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState())
+store.subscribe(rerenderEntireTree);
 
 serviceWorker.register();
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,55 +1,62 @@
-
-let rerenderEntireTree;	// просто название
-
-
-let state = {
-	postData: {
-		posts: [
-			{id: 1, message: "Hi, how are you?", likesCount: 0},
-			{id: 2, message: "It's my first post", likesCount: 34},
-			{id: 3, message: "It is my second post", likesCount: 12},
+let store = {
+	_state: {
+		postData: {
+			posts: [
+				{id: 1, message: "Hi, how are you?", likesCount: 0},
+				{id: 2, message: "It's my first post", likesCount: 34},
+				{id: 3, message: "It is my second post", likesCount: 12},
+			],
+			currentText: '',
+		},
+		messagesData: {
+			companionsData: [
+				{id: 1, name: "Sergey"},
+				{id: 2, name: "Andrey"},
+				{id: 3, name: "Askar"},
+				{id: 4, name: "Muslim"},
+				{id: 5, name: "Petr"},
+			],
+			dialogsData: [
+				{id: 1, message: "first message"},
+				{id: 2, message: "second message"},
+				{id: 3, message: "third message"},
+				{id: 4, message: "fourth message"},
+			],
+		},
+		friendsData: [
+			{id: 1, name: "friend 1", avatar: ""},
+			{id: 2, name: "friend 2", avatar: ""},
+			{id: 3, name: "friend 3", avatar: ""},
 		],
-		currentText:'',
 	},
-	messagesData: {
-		companionsData : [
-			{id:1, name:"Sergey"},
-			{id:2, name:"Andrey"},
-			{id:3, name:"Askar"},
-			{id:4, name:"Muslim"},
-			{id:5, name:"Petr"},
-		],
-		dialogsData : [
-			{id:1, message:"first message"},
-			{id:2, message:"second message"},
-			{id:3, message:"third message"},
-			{id:4, message:"fourth message"},
-		],
+
+	getState() {
+		return this._state;
 	},
-	friendsData: [
-		{id:1, name:"friend 1", avatar:""},
-		{id:2, name:"friend 2", avatar:""},
-		{id:3, name:"friend 3", avatar:""},
-	]
+
+	_callSubscriber() {
+		console.log('i am rerender')
+	},
+
+	addPost() {
+		let newMessage = {
+			id: 4,
+			message: this._state.postData.currentText,
+			likeCount: 1,
+		};
+		this._state.postData.posts.push(newMessage);
+		this._callSubscriber(this._state);
+	},
+
+	textareaChanges(newText) {
+		this._state.postData.currentText = (newText);
+		this._callSubscriber(this._state)														// rerenderEntireTree - просто название
+	},
+
+	subscribe(observer) {
+		this._callSubscriber = observer;									// rerenderEntireTree - просто название
+	},
 }
 
-export const addPost = () => {
-	let newMessage = {
-		id: 4,
-		message: state.postData.currentText,
-		likeCount: 1,
-	};
-	state.postData.posts.push(newMessage);
-	rerenderEntireTree()														// rerenderEntireTree - просто название
-}
-
-export const textareaChanges = (newText) => {
-	state.postData.currentText = (newText);
-	rerenderEntireTree()														// rerenderEntireTree - просто название
-}
-
-export const subscribe = (observer) => {
-	rerenderEntireTree = observer;									// rerenderEntireTree - просто название
-}
-
-export default state;
+export default store;
+window.store = store;
