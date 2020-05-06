b818c85fc35ba2ecd46a1639413a7bcb6fd66590
Apa6 <alim.gazdieff@gmail.com>
2020-05-06 12:48:31 +0500

    49 (element -> el in 'Users.jsx')

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,28 +3,28 @@ import s from "./Users.module.css";
 
 const Users = (props) => {
 	return  <div className={s.users}>
-			{props.users.map( (element) =>
-				<div className={s.users_items} key={element.id}>
+			{props.users.map( (el) =>
+				<div className={s.users_items} key={el.id}>
 					<div className={s.users_div}>
 						<span className={s.users_span}>
-							{element.status
-								? <button onClick={ () => {props.Follow(element.id)} }> Follow </button>
-								: <button onClick={ () => {props.unFollow(element.id)} }> Unfollow </button>
+							{el.status
+								? <button onClick={ () => {props.Follow(el.id)} }> Follow </button>
+								: <button onClick={ () => {props.unFollow(el.id)} }> Unfollow </button>
 							}
 						</span>
 						<span className={s.users_span}>
-							<img className={s.users_span_img} src={element.img} />
+							<img className={s.users_span_img} src={el.img} />
 						</span>
 					</div>
 					<div className={s.users_div}>
-						<span className={s.users_span}>{element.name}</span>
-						<span className={s.users_span}>{element.surname}</span>
+						<span className={s.users_span}>{el.name}</span>
+						<span className={s.users_span}>{el.surname}</span>
 					</div>
 					<div className={s.users_div}>
-						<span className={s.users_span}>{element.location.country}</span>
-						<span className={s.users_span}>{element.location.city}</span>
+						<span className={s.users_span}>{el.location.country}</span>
+						<span className={s.users_span}>{el.location.city}</span>
 						<br></br>
-						<span className={s.users_span}>{element.description}</span>
+						<span className={s.users_span}>{el.description}</span>
 					</div>
 				</div>
 			)}
