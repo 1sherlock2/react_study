2e3ca9d59dc4fafbd29fc6e60fe00961736483d0
Apa6 <alim.gazdieff@gmail.com>
2020-05-11 15:07:36 +0500

    60 refactor 'index.js' for jobs project

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-<<<<<<< HEAD
-import store from "./redux/State";
-
-let rerenderEntireTree = (state) => {																																										// создаем функцию, которая в качестве параметра будет передавать наши данные
-=======
 import store from "./redux/redux_store/redux_store";
 import {Provider} from "react-redux";
 
 let rerenderEntireTree = () => {																							// создаем функцию, которая в качестве параметра будет передавать наши данные
->>>>>>> Context
 	ReactDOM.render(
 		<BrowserRouter>
 			<Provider store={store}>
